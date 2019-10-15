3b278acfa0955a9bc88825c43b2d9b9893a20ce5
adilSmile <adil.110194@gmail.com>
2019-10-16 01:36:42 +0600

    printalphabet10

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,9 +1,27 @@
 package main
 
+import (
+	"os"
+	"unicode/utf8"
+	"errors"
+)
+
+func PrintRune(r rune) error {
+	l := utf8.RuneLen(r)
+	if l == -1 {
+		return errors.New("The rune is not a valid value to encode in UTF-8")
+	}
+	p := make([]byte, l)
+	utf8.EncodeRune(p, r)
+	_, err := os.Stdout.Write(p)
+	return err
+}
+
 func main() {
 	var aRune rune = 'z'
+	var endRune rune = '\n'
 	for iRune := 'a'; iRune <= aRune; iRune++ {
-		print(string(iRune))
+		PrintRune(iRune)
 	}
-	print("\n")
+	PrintRune(endRune)
 }
