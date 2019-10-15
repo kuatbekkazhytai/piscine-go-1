a92b16db96431728c18b4d52ca5c63db3a282a5f
adilSmile <adil.110194@gmail.com>
2019-10-16 04:07:47 +0600

    printcomp 2

---
+++ printcomb2.go
@@ -0,0 +1,26 @@
+package printcomb2
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := 0; i <= 9; i++ {
+		for j := 0; j <= 9; j++ {
+			for k := 0; k <= 9; k++ {
+				for l := 0; l <= 9; l++ {
+					if i * 10 + j < k * 10 + l {
+						z01.PrintRune(rune(i))
+						z01.PrintRune(rune(j))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(k))
+						z01.PrintRune(rune(l))
+						if i != 9 || j != 8 || k != 9 || l != 9 {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
