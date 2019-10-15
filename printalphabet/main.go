015b3669e69b5dfab10696f2b3277b8a118b8383
adilSmile <adil.110194@gmail.com>
2019-10-16 01:41:09 +0600

    printalphabet12

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"errors"
-	"os"
-	"unicode/utf8"
+	"fmt"
 )
 
-func PrintRune(r rune) error {
-	l := utf8.RuneLen(r)
-	if l == -1 {
-		return errors.New("The rune is not a valid value to encode in UTF-8")
-	}
-	p := make([]byte, l)
-	utf8.EncodeRune(p, r)
-	_, err := os.Stdout.Write(p)
-	return err
-}
-
-func main() {
-	var aRune rune = 'z'
-	var endRune rune = '\n'
-	for iRune := 'a'; iRune <= aRune; iRune++ {
-		PrintRune(iRune)
-	}
-	PrintRune(endRune)
+func main(){
+	fmt.Println("abcdefghijklmnopqrstuvwxyz")
 }
