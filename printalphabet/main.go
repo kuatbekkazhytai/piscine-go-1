a8e0a13a5de5474c6e0b4fc5e63b6fd192643ae2
adilSmile <adil.110194@gmail.com>
2019-10-16 02:14:19 +0600

    printalphabet17

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,10 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune rune = 'z'
-	var endRune rune = '\n'
-	for iRune := 'a'; iRune <= aRune; iRune++ {
+	for iRune := 'a'; iRune <= 'z'; iRune++ {
 		z01.PrintRune(iRune)
 	}
-	z01.PrintRune(endRune)
+	z01.PrintRune('\n')
 }
