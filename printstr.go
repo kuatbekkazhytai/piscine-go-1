c2f1d2ef802b1f95fbf38d3509d23e86d019aa92
snooprogg <adil.110194@gmail.com>
2019-10-17 01:27:16 +0600

    printstr_4

--- printstr.go
+++ printstr.go
@@ -3,7 +3,7 @@ package printstr
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	bytes := []byte(str)
+	bytes := []rune(str)
 	for i, v := range bytes {
 		z01.PrintRune(bytes[i])
 	}
