26d6eaec0999467e1669f9e93c9468db74039f45
snooprogg <adil.110194@gmail.com>
2019-10-17 04:04:08 +0600

    basicatoi_07

--- basicatoi.go
+++ basicatoi.go
@@ -3,36 +3,27 @@ package basicatoi
 func BasicAtoi(s string) int {
 	runes := []rune(s)
 	var res int = 0
-	for r := range runes {
+	for _, r := range runes {
 		res *= 10
 		if r == '0' {
 			res += 0
-		}
-		if r == '1' {
+		} else if r == '1' {
 			res += 1
-		}
-		if r == '2' {
+		} else if r == '2' {
 			res += 2
-		}
-		if r == '3' {
+		} else if r == '3' {
 			res += 3
-		}
-		if r == '4' {
+		} else if r == '4' {
 			res += 4
-		}
-		if r == '5' {
+		} else if r == '5' {
 			res += 5
-		}
-		if r == '6' {
+		} else if r == '6' {
 			res += 6
-		}
-		if r == '7' {
+		} else if r == '7' {
 			res += 7
-		}
-		if r == '8' {
+		} else if r == '8' {
 			res += 8
-		}
-		if r == '9' {
+		} else if r == '9' {
 			res += 9
 		}
 	}
