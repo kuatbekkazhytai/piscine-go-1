c113e6deb464e46248b6169e9952a5f62b2b9b32
snooprogg <adil.110194@gmail.com>
2019-10-17 02:24:31 +0600

    strlen_01

--- strlen.go
+++ strlen.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func StrLen(str string) int {
 	runes := []rune(str)
 	var count int = 0
-	for i, _ := range runes {
-		count += i
+	for i := range runes {
+		count++
 	}
 	return count
 }
