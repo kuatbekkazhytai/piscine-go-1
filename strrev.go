5a5935c8d5fa493913839c0cbd36e8c1d2f1a903
snooprogg <adil.110194@gmail.com>
2019-10-17 03:00:43 +0600

    strrev_01

--- strrev.go
+++ strrev.go
@@ -1,17 +1,17 @@
 package strrev
 
 func StrRev(s string) string {
-	runes = []rune(s)
+	runes := []rune(s)
 	var len int = 0
+	var tempRune rune
 	for v := range runes {
 		len++
 		v = v
 	}
-	var resRunes [len]rune
-	j := len - 1
-	for i := 0; i < len; i++ {
-		resRunes[i] = runes[j]
-		j--
+	for i := 0; i <= len/2; i++ {
+		tempRune = runes[i]
+		runes[i] = runes[i*2]
+		runes[i*2] = tempRune
 	}
-	return string(resRunes)
+	return string(runes)
 }
