c411a664d0d0b0e22bbfb70ffccb3fb393605616
snooprogg <adil.110194@gmail.com>
2019-10-17 16:29:10 +0600

    zaparilsya uzhe

--- printnbr.go
+++ printnbr.go
@@ -7,9 +7,10 @@ func PrintNbr(n int) {
 		z01.PrintRune('-')
 		if n == -9223372036854775808 {
 			z01.PrintRune('9')
-			n = -223372036854775808
+			n = 223372036854775808
+		} else {
+			n *= -1
 		}
-		n *= -1
 	} else {
 		if n > 9 {
 			PrintNbr(n / 10)
