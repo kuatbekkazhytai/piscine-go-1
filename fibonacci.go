1a9bfa3dfac267c1328defc85182a8886a7be352
snooprogg <adil.110194@gmail.com>
2019-10-21 01:51:09 +0600

    fib_rec

--- fibonacci.go
+++ fibonacci.go
@@ -3,15 +3,11 @@ package piscine
 func Fibonacci(index int) int {
 	if index < 0 {
 		return -1
+	} else if index == 0 {
+		return 0
+	} else if index == 1 {
+		return 1
 	} else {
-		n1 := 0
-		n2 := 1
-		t := 0
-		for i := 0; i < index; i++ {
-			t = n2 + n1
-			n1 = n2
-			n2 = t
-		}
-		return n1
+		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
 }
