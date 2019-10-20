f8bebcb71889c2f1a2053911f51d7df546068e16
snooprogg <adil.110194@gmail.com>
2019-10-21 01:31:58 +0600

    sqrt

---
+++ sqrt.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	res := 0
+	for i := 0; i*i <= nb; i++ {
+		res = i
+	}
+	if res*res != nb {
+		return 0
+	} else {
+		return nb
+	}
+}
